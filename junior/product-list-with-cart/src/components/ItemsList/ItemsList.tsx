077e00f86932ad951bb49a79a80b22b344d14ff8
Brian Esteban <briangesteban@gmail.com>
2024-09-16 09:54:16 -0700

    Add Add to Cart button

--- junior/product-list-with-cart/src/components/ItemsList/ItemsList.tsx
+++ junior/product-list-with-cart/src/components/ItemsList/ItemsList.tsx
@@ -2,14 +2,17 @@ import menuData from "../../data/data.json";
 import "./ItemsList.scss";
 
 const ItemsList = () => {
-  const menuCards = menuData.map((menuItem) => {
+  const menuCards = menuData.map((menuItem, index) => {
     return (
-      <article className="card">
-        <img
-          src={menuItem.image.mobile}
-          alt={menuItem.name}
-          className="card__item-img"
-        />
+      <article className="card" key={index}>
+        <div className="card__img-container">
+          <img
+            src={menuItem.image.mobile}
+            alt={menuItem.name}
+            className="card__item-img"
+          />
+          <button className="card__btn-atc">Add to Cart</button>
+        </div>
         <p className="card__item-category">{menuItem.category}</p>
         <h2 className="card__item-name">{menuItem.name}</h2>
         <h3 className="card__item-price">${menuItem.price.toFixed(2)}</h3>
