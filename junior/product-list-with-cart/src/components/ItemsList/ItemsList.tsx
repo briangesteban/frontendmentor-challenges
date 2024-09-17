ca7858fb4fecac1f902757aa5b0a22a6eb4e52f2
Brian Esteban <briangesteban@gmail.com>
2024-09-17 12:16:07 -0700

    Add button icons and counter buttons

--- junior/product-list-with-cart/src/components/ItemsList/ItemsList.tsx
+++ junior/product-list-with-cart/src/components/ItemsList/ItemsList.tsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
 import menuData from "../../data/data.json";
+import AddToCartIcon from "../../assets/images/icon-add-to-cart.svg";
+import IncreaseIcon from "../../assets/images/icon-increment-quantity.svg";
+import DecreaseIcon from "../../assets/images/icon-decrement-quantity.svg";
 import "./ItemsList.scss";
 
 const ItemsList = () => {
+  // Cart State
+  const [isCartEmpty, setIsCartEmpty] = useState<boolean>(true);
+
+  // Add to Cart Button Handler
+  const addToCartHandler = (): void => {
+    setIsCartEmpty(false);
+  };
+
   const menuCards = menuData.map((menuItem, index) => {
     return (
       <article className="card" key={index}>
@@ -9,9 +21,47 @@ const ItemsList = () => {
           <img
             src={menuItem.image.mobile}
             alt={menuItem.name}
-            className="card__item-img"
+            className={
+              !isCartEmpty
+                ? "card__item-img card__item-img--border-on"
+                : "card__item-img"
+            }
           />
-          <button className="card__btn-atc">Add to Cart</button>
+          <button
+            className={
+              !isCartEmpty
+                ? "card__btn-atc card__btn-atc--hidden"
+                : "card__btn-atc"
+            }
+            onClick={() => {
+              addToCartHandler();
+            }}
+          >
+            <img
+              src={AddToCartIcon}
+              alt="Add to cart icon"
+              className="card__icon-atc"
+            />
+            Add to Cart
+          </button>
+          <div
+            className={
+              !isCartEmpty
+                ? "card__item-counter card__item-counter--visible"
+                : "card__item-counter"
+            }
+            aria-label={`${menuItem.name} Count`}
+          >
+            <button className="card__btn-counter" aria-label="Decrease value">
+              <img src={DecreaseIcon} alt="Decrease icon" />
+            </button>
+            <span className="card__item-count" aria-label="Item Count">
+              1
+            </span>
+            <button className="card__btn-counter" aria-label="Increase value">
+              <img src={IncreaseIcon} alt="Increase icon" />
+            </button>
+          </div>
         </div>
         <p className="card__item-category">{menuItem.category}</p>
         <h2 className="card__item-name">{menuItem.name}</h2>
