849736724a14ddc6a1951079dc4a8170acdf3d89
Brian Esteban <briangesteban@gmail.com>
2024-09-17 13:22:19 -0700

    Update elements spacing

--- junior/product-list-with-cart/src/components/ItemBoard/ItemBoard.tsx
+++ junior/product-list-with-cart/src/components/ItemBoard/ItemBoard.tsx
@@ -4,13 +4,13 @@ import "./ItemBoard.scss";
 
 const ItemBoard = () => {
   return (
-    <section className="item-board">
+    <main className="item-board">
       <h1 className="item-board__header">Desserts</h1>
       <section className="item-board__menu-list">
         <ItemsList />
         <CartPreview />
       </section>
-    </section>
+    </main>
   );
 };
 
