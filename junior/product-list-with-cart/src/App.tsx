d099272510ac8d5e5fed3a401f38031570ad4819
Brian Esteban <briangesteban@gmail.com>
2024-09-27 17:58:55 -0700

    Add routes

--- junior/product-list-with-cart/src/App.tsx
+++ junior/product-list-with-cart/src/App.tsx
@@ -1,8 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
 import { ItemBoard } from "./components/ItemBoard/ItemBoard";
 
 const App = () => {
-  return <ItemBoard />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={import.meta.env.BASE_URL} element={<ItemBoard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
